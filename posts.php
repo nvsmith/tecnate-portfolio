d9d97c242593102daff395c77ea88f632eeb3f59
Nathan Smith <nathanielvs@gmail.com>
2024-10-16 11:19:05 -0600

    Reformat card into sections and overlays for better control of styling. Streamline classnames to improve naming consistency and structural hierarchy.

--- posts.php
+++ posts.php
@@ -54,80 +54,83 @@ if( $the_query->have_posts() ) :
             'Bash' => 'portfoliobash'
         ];?>
 
-        <!-- Render post in a 6/12 column layout until the md breakpoint -->
-        <div class="col-md-6 col--pad">
+       <!-- Render post in a 6/12 column layout until the md breakpoint -->
+       <div class="col-md-6 col--pad">
             <div class="card">
                 <div class="card__mat">
-                    <div class="card__content-wrapper">
-                        <div class="card__img-wrapper"
-                            style="background-image: url(<?php echo esc_url( $primary_image['url'] ); ?>);">
+                    <div class="card__body">
+                        <div class="card-hero">
+                            <!-- Display Primary Image -->
+                            <?php if ($primary_image) : ?>
+                                <img src="<?php echo esc_url($primary_image['url']); ?>" alt="<?php echo esc_attr($project_title); ?>" class="card-hero__img">
+                            <?php endif; ?>
+                        </div> <!-- end card-hero -->
                         
-                            <div class="card__title-container">
-                                <div class="card__role-wrapper">
-                                    <!-- Display Primary Role Icon -->
-                                    <?php if (isset($role_icons[$primary_role])) : ?>
-                                        <svg class="ct-fancy-icon card__role-icon"><use xlink:href="#<?php echo esc_attr($role_icons[$primary_role]); ?>"><use></svg>
-                                    <?php endif; ?>
-                                </div>
-
-                                <div class="card__title-wrapper">
-                                    <!-- Display Project Title or fallback to post title -->
-                                    <h5><?php echo esc_html($project_title ? $project_title : get_the_title()); ?></h5>
-                                </div>
-
-                                <div class="card__technology-wrapper">
-                                    <!-- Display Primary Technology Icon -->
-                                    <?php if (isset($technology_icons[$primary_technology])) : ?>
-                                        <svg class="ct-fancy-icon card__technology-icon"><use xlink:href="#<?php echo esc_attr($technology_icons[$primary_technology]); ?>"></use></svg>
-                                    <?php endif; ?>
-                                </div>
-                            </div>
-    
-                            <div class="card__language-container">
-                                <!-- Display language icons -->
-                                <?php foreach ([$language1, $language2, $language3] as $language) :
-                                    if ($language && array_key_exists($language, $language_icons)) : ?>
-                                    <div class="card__language-icon-wrapper">
-                                        <svg class="ct-fancy-icon card__language-icon"><use xlink:href="#<?php echo esc_attr($language_icons[$language]); ?>"></use></svg>
-                                    </div>
-                                    <?php endif;
-                                endforeach; ?>
-                            </div>
-
-                        </div>
-    
-                        <div class="card__category-wrapper">
-                            <!-- Display Development Category -->
-                            <?php if ($primary_development_category) : ?>
-                                <h4 class="card__development">Category:<?php echo esc_html($primary_development_category); ?></h4>
-                            <?php endif; ?>
-    
-                            <!-- Display Project Category -->
-                            <?php if ($primary_project_category) : ?>
-                                <h4 class="card__project">Type: <?php echo esc_html($primary_project_category); ?></h4>
-                            <?php endif; ?>
-                        </div>
-    
-                        <div class="card__summary-wrapper">
+                        <div class="card-summary">
                             <!-- Display Card Summary or fallback to post excerpt -->
-                            <?php if ($card_summary) : ?>
-                                <p><?php echo esc_html($card_summary); ?></p>
-                            <?php else : ?>
-                                <p><?php the_excerpt(); ?></p> <!-- Fallback to post excerpt -->
-                            <?php endif; ?>
+                            <p><?php echo esc_html($card_summary ?: get_the_excerpt()); ?></p>
     
                             <div class="card__btn">
                                 <a href="<?php the_permalink(); ?>">View Project</a>
                             </div>
-                        </div>
-                    </div>
+                        </div> <!-- end card-summary -->
+                        
+                        <div class="card-footer">
+                            <?php
+                            $post_date = get_the_date('d M Y');
+                            echo '<p class="card-footer__date">Published: ' . esc_html($post_date) . '</p>';
+                            ?>
+                        </div> <!-- end card-footer -->
 
-                    <div class="card__footer"></div>
-                </div>
-            </div>
+                        <div class="overlay__title-container">
+                            <div class="card-icon__wrapper card-icon__wrapper--role">
+                                <!-- Display Primary Role Icon -->
+                                <?php if (isset($role_icons[$primary_role])) : ?>
+                                    <svg class="card-icon card-icon--role"><use xlink:href="#<?php echo esc_attr($role_icons[$primary_role]); ?>"></use></svg>
+                                <?php endif; ?>
+                            </div>
+
+                            <div class="wrapper__title">
+                                <!-- Display Project Title or fallback to post title -->
+                                <h5><?php echo esc_html($project_title ?: get_the_title()); ?></h5>
+                            </div>
+
+                            <div class="card-icon__wrapper card-icon__wrapper--technology">
+                                <!-- Display Primary Technology Icon -->
+                                <?php if (isset($technology_icons[$primary_technology])) : ?>
+                                    <svg class="card-icon card-icon--technology"><use xlink:href="#<?php echo esc_attr($technology_icons[$primary_technology]); ?>"></use></svg>
+                                <?php endif; ?>
+                            </div>
+                        </div> <!-- end overlay__title-container -->
+
+                        <div class="overlay__language-container">
+                            <!-- Display language icons -->
+                            <?php foreach ([$language1, $language2, $language3] as $language) :
+                                if ($language && array_key_exists($language, $language_icons)) : ?>
+                                    <div class="card-icon__wrapper card-icon__wrapper--language">
+                                        <svg class="card-icon card-icon--language"><use xlink:href="#<?php echo esc_attr($language_icons[$language]); ?>"></use></svg>
+                                    </div>
+                                <?php endif;
+                            endforeach; ?>
+                        </div> <!-- end overlay__language-container -->
+
+                        <div class="overlay__category-container">
+                            <!-- Display Development Category -->
+                            <?php if ($primary_development_category) : ?>
+                                <h4 class="card__development">Category: <span><?php echo esc_html($primary_development_category); ?></span></h4>
+                            <?php endif; ?>
+    
+                            <!-- Display Project Category -->
+                            <?php if ($primary_project_category) : ?>
+                                <h4 class="card__project">Type: <span><?php echo esc_html($primary_project_category); ?></span></h4>
+                            <?php endif; ?>
+                        </div> <!-- end overlay__category-container -->
+                    </div> <!-- end card__body -->
+                </div> <!-- end card__mat -->
+            </div> <!-- end card -->
         </div> <!-- end col-md-6 | End post rendering --> 
 
-<?php
+    <?php
     endwhile;
     echo '</div>'; // end posts-row
     echo '</div>'; // end posts-container
