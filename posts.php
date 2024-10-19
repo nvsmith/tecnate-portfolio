4eba3e343b012fa73a58625b03bddec999f3a35d
Nathan Smith <nathanielvs@gmail.com>
2024-10-18 18:38:57 -0600

    Simplify BEM classnames.

--- posts.php
+++ posts.php
@@ -54,83 +54,84 @@ if( $the_query->have_posts() ) :
             'Bash' => 'portfoliobash'
         ];?>
 
-       <!-- Render post in a 6/12 column layout until the md breakpoint -->
-       <div class="col__card col-md-6">
+        <!-- Render post in a 6/12 column layout until the md breakpoint -->
+        <div class="col-md-6 col__card">
             <div class="card">
                 <div class="card__mat">
                     <div class="card__body">
 
                         <!-- BEGIN BASE SECTIONS -->
-                        <div class="card-hero" style = "background-image: url('<?php echo esc_url($primary_image['url']); ?>')">
-                            <div class="wrapper__project-title">
-                                <!-- Display Project Title or fallback to post title -->
-                                <h2 class="card-title"><?php echo esc_html($project_title ?: get_the_title()); ?></h2>
+                        <div class="card__hero" style="background-image: url('<?php echo esc_url($primary_image['url']); ?>')">
+                            <div class="card__title-wrapper">
+                                <h2 class="card__title"><?php echo esc_html($project_title ?: get_the_title()); ?></h2>
                             </div>
-                        </div> <!-- end card-hero -->
+                        </div> <!-- end card__hero -->
 
-                        <div class="card-categories">
+                        <div class="card__categories">
                             <!-- Display Development Category -->
                             <?php if ($primary_development_category) : ?>
-                                <h4 class="card__development">Category: <span><?php echo esc_html($primary_development_category); ?></span></h4>
+                                <h4 class="card__category card__category--development">Category: 
+                                    <span><?php echo esc_html($primary_development_category); ?></span>
+                                </h4>
                             <?php endif; ?>
-    
+
                             <!-- Display Project Category -->
                             <?php if ($primary_project_category) : ?>
-                                <h4 class="card__project">Type: <span><?php echo esc_html($primary_project_category); ?></span></h4>
+                                <h4 class="card__category card__category--project">Type: 
+                                    <span><?php echo esc_html($primary_project_category); ?></span>
+                                </h4>
                             <?php endif; ?>
-                        </div> <!-- end card-categories -->
-                        
-                        <div class="card-summary">
-                            <!-- Display Card Summary or fallback to post excerpt -->
+                        </div> <!-- end card__categories -->
+
+                        <div class="card__summary">
                             <p><?php echo esc_html($card_summary ?: get_the_excerpt()); ?></p>
-    
                             <div class="card__btn">
                                 <a href="<?php the_permalink(); ?>">View Project</a>
                             </div>
-                        </div> <!-- end card-summary -->
-                        
-                        <div class="card-footer">
-                            <?php
-                            $post_date = get_the_date('d M Y');
-                            echo '<p class="card-footer__date">Published: ' . esc_html($post_date) . '</p>';
-                            ?>
-                        </div> <!-- end card-footer -->
+                        </div> <!-- end card__summary -->
+
+                        <div class="card__footer">
+                            <time class="card__footer-date" datetime="<?php echo get_the_date('c'); ?>">Published: <?php echo get_the_date('d M Y'); ?></time>
+                        </div> <!-- end card__footer -->
                         <!-- END BASE SECTIONS -->
 
-                        
                         <!-- BEGIN OVERLAY SECTIONS -->
-                        <div class="card-overlay card-overlay__role-container">
-                            <div class="card-icon__wrapper card-icon__wrapper--role">
-                                <!-- Display Primary Role Icon -->
+                        <div class="card__overlay card__overlay--role">
+                            <div class="card__icon-wrapper card__icon-wrapper--role">
                                 <?php if (isset($role_icons[$primary_role])) : ?>
-                                    <svg class="card-icon card-icon--role"><use xlink:href="#<?php echo esc_attr($role_icons[$primary_role]); ?>"></use></svg>
+                                    <svg class="card__icon card__icon--role">
+                                        <use xlink:href="#<?php echo esc_attr($role_icons[$primary_role]); ?>"></use>
+                                    </svg>
                                 <?php endif; ?>
                             </div>
 
-                            <div class="card-icon__wrapper card-icon__wrapper--technology">
-                                <!-- Display Primary Technology Icon -->
+                            <div class="card__icon-wrapper card__icon-wrapper--technology">
                                 <?php if (isset($technology_icons[$primary_technology])) : ?>
-                                    <svg class="card-icon card-icon--technology"><use xlink:href="#<?php echo esc_attr($technology_icons[$primary_technology]); ?>"></use></svg>
+                                    <svg class="card__icon card__icon--technology">
+                                        <use xlink:href="#<?php echo esc_attr($technology_icons[$primary_technology]); ?>"></use>
+                                    </svg>
                                 <?php endif; ?>
                             </div>
-                        </div> <!-- end overlay__role-container -->
+                        </div> <!-- end card__overlay--role -->
 
-                        <div class="card-overlay card-overlay__language-container">
-                            <!-- Display language icons -->
+                        <div class="card__overlay card__overlay--languages">
                             <?php foreach ([$language1, $language2, $language3] as $language) :
                                 if ($language && array_key_exists($language, $language_icons)) : ?>
-                                    <div class="card-icon__wrapper card-icon__wrapper--language">
-                                        <svg class="card-icon card-icon--language"><use xlink:href="#<?php echo esc_attr($language_icons[$language]); ?>"></use></svg>
+                                    <div class="card__icon-wrapper card__icon-wrapper--language">
+                                        <svg class="card__icon card__icon--language">
+                                            <use xlink:href="#<?php echo esc_attr($language_icons[$language]); ?>"></use>
+                                        </svg>
                                     </div>
                                 <?php endif;
                             endforeach; ?>
-                        </div> <!-- end overlay__language-container -->                        
+                        </div> <!-- end card__overlay--languages -->
                         <!-- END OVERLAY SECTIONS -->
 
                     </div> <!-- end card__body -->
                 </div> <!-- end card__mat -->
             </div> <!-- end card -->
-        </div> <!-- end col__card | End post rendering --> 
+        </div> <!-- end col__card -->
+
 
     <?php
     endwhile;
