5b723beb16573008ce57753e8ee60854ce83d2fe
Nathan Smith <nathanielvs@gmail.com>
2024-11-05 21:23:33 -0700

    Add container and row structure to sandbox code.

--- portfolio-posts-sandbox.php
+++ portfolio-posts-sandbox.php
@@ -12,58 +12,72 @@ $portfolio_query = new WP_Query(array(
     'orderby' => 'date'
 ));
 
-// Start The Loop to render each post
+echo '<div id="posts-container" class="container">';
+
+// Start The Loop to fetch posts
 if ($portfolio_query) :
     if ($portfolio_query->have_posts()) :
 
         // Top Pagination
-        $big = 999999999; // Need an unlikely placeholder in the base parameter for the paginate_links function
-        echo paginate_links(array(
-            'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))), // Structure URL for pagination, using a placeholder for the page number
-            'format' => '/page/%#%/', // Format URL structure to match Oxygen defaults
-            'current' => max(1, get_query_var('paged')), // Current page number
-            'total' => $portfolio_query->max_num_pages, // Total number of pages
-            'prev_text' => esc_attr__('&larr; Previous'), // Previous page link
-            'next_text' => esc_attr__('Next &rarr;'), // Next page link
-        ));
+        echo '<div id="top-pagination" class="row row__pagination">';
+            $big = 999999999; // Placeholder in the base parameter for the paginate_links function
+            echo paginate_links(array(
+                'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))), // Structure URL for pagination, using a placeholder for the page number
+                'format' => '/page/%#%/', // Format URL structure to match Oxygen defaults
+                'current' => max(1, get_query_var('paged')), // Current page number
+                'total' => $portfolio_query->max_num_pages, // Total number of pages
+                'prev_text' => esc_attr__('&larr; Previous'), // Previous page link
+                'next_text' => esc_attr__('Next &rarr;'), // Next page link
+            ));
+        echo '</div>'; // end top pagination
 
-        while ($portfolio_query->have_posts()) : $portfolio_query->the_post();
-            // Retrieve ACF/SCF fields for base sections
-            $project_title = get_field('project_title');
-            $primary_image = get_field('primary_image');
-            $primary_development_category = get_field('primary_development_category');
-            $primary_project_category = get_field('primary_project_category');
-            $card_summary = get_field('card_summary');
+        echo '<div id="posts-row" class="row">';
+            while ($portfolio_query->have_posts()) : $portfolio_query->the_post();
+                // Retrieve ACF/SCF fields for base sections
+                $project_title = get_field('project_title');
+                $primary_image = get_field('primary_image');
+                $primary_development_category = get_field('primary_development_category');
+                $primary_project_category = get_field('primary_project_category');
+                $card_summary = get_field('card_summary');
 
-            // Echo each ACF/SCF field value in <p> tags, but only if the field exists and has a value
-            if ($project_title) echo '<p>' . $project_title . '</p>';
-            if ($primary_image) echo '<p>' . $primary_image . '</p>';
-            if ($primary_development_category) echo '<p>' . $primary_development_category . '</p>';
-            if ($primary_project_category) echo '<p>' . $primary_project_category . '</p>';
-            if ($card_summary) echo '<p>' . $card_summary . '</p>';
-            echo '<hr>';
+                // Echo each ACF/SCF field value in <p> tags, but only if the field exists and has a value
+                if ($project_title) echo '<p>' . $project_title . '</p>';
+                if ($primary_image) echo '<p>' . $primary_image . '</p>';
+                if ($primary_development_category) echo '<p>' . $primary_development_category . '</p>';
+                if ($primary_project_category) echo '<p>' . $primary_project_category . '</p>';
+                if ($card_summary) echo '<p>' . $card_summary . '</p>';
+                echo '<hr>';
 
-        endwhile;
+            endwhile;
+        echo '</div>'; // end posts-row
 
         // Bottom Pagination
-        echo paginate_links(array(
-            'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))), // Structure URL for pagination, using a placeholder for the page number
-            'format' => '/page/%#%/', // Format URL structure to match Oxygen defaults
-            'current' => max(1, get_query_var('paged')), // Current page number
-            'total' => $portfolio_query->max_num_pages, // Total number of pages
-            'prev_text' => esc_attr__('&larr; Previous'), // Previous page link
-            'next_text' => esc_attr__('Next &rarr;'), // Next page link
-        ));
+        echo '<div id="bottom-pagination" class="row row__pagination">';
+            $big = 999999999; // Placeholder in the base parameter for the paginate_links function
+            echo paginate_links(array(
+                'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))), // Structure URL for pagination, using a placeholder for the page number
+                'format' => '/page/%#%/', // Format URL structure to match Oxygen defaults
+                'current' => max(1, get_query_var('paged')), // Current page number
+                'total' => $portfolio_query->max_num_pages, // Total number of pages
+                'prev_text' => esc_attr__('&larr; Previous'), // Previous page link
+                'next_text' => esc_attr__('Next &rarr;'), // Next page link
+            ));
+        echo '</div>'; // end bottom pagination
 
         // Reset post data to prevent conflicts with other loops
         wp_reset_postdata();
 
     else :
-        echo '<p>Sorry, there are no posts available.</p>';
+        echo 
+        '<div id="no-posts-row" class="row">
+            <p id="no-posts" class="error">Sorry, there are no posts available.</p>
+        </div>';
     endif;
 
 else : 
-    echo '<p>Query Failed</p>';
+    echo '<p id="query-failed" class="error">Query Failed</p>';
 endif;
+
+echo '</div>'; // end posts-container
 
 ?>
